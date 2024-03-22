67817c4da0bfdf014135aa100a0167c945e7afc8
devMichaelKimani <mashkim854@gmail.com>
2024-03-22 08:49:13 +0300

    controller, model, route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class TaskController extends Controller
+{
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse{
+        return response()->json(Task::all());
+
+}
+}

133bafcfbd694ee21d88895d06c0bc846b80a997
devMichaelKimani <mashkim854@gmail.com>
2024-03-22 09:20:28 +0300

    update Task Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $casts = [
+        'is_done' => 'boolean'
+    ];
+    protected $hidden =[
+        'updated_at'
+    ];
 }

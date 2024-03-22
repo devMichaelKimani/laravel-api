9c22155e5f7ca37fc6b91eb1d4a5b9d527210489
devMichaelKimani <mashkim854@gmail.com>
2024-03-22 10:59:52 +0300

    relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Task extends Model
 {
@@ -15,4 +17,8 @@ class Task extends Model
     protected $hidden =[
         'updated_at'
     ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
